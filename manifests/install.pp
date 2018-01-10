543ed0fdb7004c45d5217857364f3efee8e5a5eb
ales jurica <alesj@ales-debian>
2018-01-10 12:21:13 +0100

    Changing the way the influxdb installation is being handled.
    Adding support for Debian.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,10 +2,26 @@
 class influxdb::install(
   $version,
 ){
-  package { 'influxdb_rpm':
-    ensure   => installed,
-    name     => 'influxdb',
-    provider => 'rpm',
-    source   => "https://dl.influxdata.com/influxdb/releases/influxdb-${version}.x86_64.rpm"
+  case $::osfamily {
+    'Debian': {
+      package { 'influxdb_deb':
+        ensure   => installed,
+        name     => 'influxdb',
+        provider => 'dpkg',
+        source   => "https://dl.influxdata.com/influxdb/releases/influxdb_${version}_amd64.deb"
+      }
+    }
+    'RedHat': {
+      package { 'influxdb_rpm':
+        ensure   => installed,
+        name     => 'influxdb',
+        provider => 'rpm',
+        source   => "https://dl.influxdata.com/influxdb/releases/influxdb-${version}.x86_64.rpm"
+      }
+    }
+    default: {
+      fail("Unsupported managed repository for osfamily: ${::osfamily}, operatingsystem: ${::operatingsystem}, module ${module_name} currently only supports managing repos for osfamily RedHat and Debian")
+    }
   }
+
 }
