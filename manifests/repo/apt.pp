ac2f8a6bb8961b5b2a026274b893c076d19e7928
Silvia Alvaro <silvia@brandwatch.com>
2018-09-12 13:03:27 +0200

    fixed syntax and indentation errors

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -6,15 +6,15 @@ class influxdb::repo::apt {
   $_operatingsystem = downcase($::operatingsystem)
 
   apt::source { 'repos.influxdata.com':
-    location    => "https://repos.influxdata.com/${_operatingsystem}",
-    release     => $::lsbdistcodename,
-    repos       => 'stable',
-    key         => {
-	'id'	  => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-    	'source'  => 'https://repos.influxdata.com/influxdb.key',
-    }
-    include =>  {
-	'src'     => false,
+    location => "https://repos.influxdata.com/${_operatingsystem}",
+    release  => $::lsbdistcodename,
+    repos    => 'stable',
+    key      => {
+      'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+      'source' => 'https://repos.influxdata.com/influxdb.key',
+    },
+    include  =>  {
+      'src'    => false,
     }
   }
 
